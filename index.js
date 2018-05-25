ae62beca195a2d7fcf97c7020c9a404fff12fef4
Tim Callies <call3675@stthomas.edu>
2018-05-25 14:56:20 -0500

    Added .gitignore

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
